c79f3f066d10a3bf756c4701af7a62e4ee650602
ivansanchezvera <ivansanchez_1988@hotmail.com>
2018-04-06 15:44:43 -0500

    Pointcuts: Using pointcut expressions to apply several advices to the same method. Pointcuts let you define the point in which the advice will be applied and it is reusable (The pointcut expression).

--- src/ec/edu/upse/facsistel/spring/aop/aspect/LoggingAspect.java
+++ src/ec/edu/upse/facsistel/spring/aop/aspect/LoggingAspect.java
@@ -2,13 +2,26 @@ package ec.edu.upse.facsistel.spring.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public * get*())")
+	@Before("allGetters()")
 	public void loggingAdvice()
 	{
 		System.out.println("Advice run. Get Method called.");
 	}
+	
+	@Before("allGetters()")
+	public void secondAdvice()
+	{
+		System.out.println("Second advice mutherfucker!");
+	}
+	
+	//This method is a dummy method, used to apply the point cut.
+	//It kinda holds the pointcut.
+	@Pointcut("execution(* get*())")
+	public void allGetters() {}
+	
 }
